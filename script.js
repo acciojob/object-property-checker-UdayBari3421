1657f3feb72fb04579b59a1947d6957fb23cb851
UdayBari3421 <115573575+UdayBari3421@users.noreply.github.com>
2023-09-21 21:39:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
-  //   write your code here
+	for(let i = 0; i < sampleObject.length; i++){
+		if(key === sampleObject[i]){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
 }
 
 // Do not change the code below
-const key = prompt("Enter Key.");
-alert(hasKey(key));
+// const key = prompt("Enter Key.");
+// alert(hasKey(key));
