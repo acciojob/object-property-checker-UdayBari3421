6a4da6fe0ca82c4ed8d8fd44084d80201d28b138
UdayBari3421 <115573575+UdayBari3421@users.noreply.github.com>
2023-09-22 10:25:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
-	
+	for (let keys in sampleObject) {
+		if(keys == key){ 
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
 }
 
 // Do not change the code below
